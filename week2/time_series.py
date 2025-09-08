c16dd622e71fc2e76ca4a116520042d77a6e41d2
soyeeee2 <soyeeee2@gmail.com>
2025-09-08 18:24:41 +0900

    week22

--- week2/time_series.py
+++ week2/time_series.py
@@ -3,13 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
-# ===== 한글 폰트 설정 (macOS 기준) =====
 font_path = '/Users/soyee/Library/Fonts/NanumGothic-Regular.ttf'
 fontprop = fm.FontProperties(fname=font_path)
 plt.rc('font', family=fontprop.get_name())
 plt.rc('axes', unicode_minus=False)
 
-# ===== 시계열 데이터 생성 =====
 date_index = pd.date_range(start='2025-09-08', periods=200, freq='D')
 ts = pd.DataFrame({
     'date': date_index,
@@ -17,13 +15,10 @@ ts = pd.DataFrame({
 })
 ts = ts.set_index('date')
 
-# ===== 주간 평균 =====
 weekly = ts.resample('W').mean()
 
-# ===== 7일간 평균 (롤링) =====
 rolling7 = ts.rolling(window=7, min_periods=1).mean()
 
-# ===== 시각화 =====
 plt.figure()
 plt.plot(ts.index, ts['value'], label='일간 값')
 plt.title('일간 시계열 값', fontproperties=fontprop)
